f54d5aef5b59cf64734ec7f416bdd642caceef7e
Juan Camilo <juancmld@gmail.com>
2021-10-24 22:24:22 -0500

    auth0

---
+++ src/Componentes/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {Auth0Provider, useAuth0} from '@auth0/auth0-react';
+
+export const LoginButton=()=>{
+    const {loginWithRedirect}= useAuth0();
+console.log("entro");
+
+    return <button onClick={()=>loginWithRedirect()}>Login</button>
+}
